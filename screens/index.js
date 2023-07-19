1a189a99bca43ae15c56b9563f0ffaaaabab4a37
DawidBubernak <92649325+BuberDev@users.noreply.github.com>
2023-07-19 08:47:01 +0200

    Create index.js

---
+++ screens/index.js
@@ -0,0 +1,7 @@
+import Login from './Login';
+import Welcome from './Welcome';
+import Signup from './Signup';
+import MainScreen from './MainScreen';
+import Chat from './Chat';
+
+export { Login, Welcome, Signup, MainScreen, Chat };
