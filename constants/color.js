4fc472eb4613fac447439cd025049fd49a35388a
DawidBubernak <92649325+BuberDev@users.noreply.github.com>
2023-07-19 08:45:24 +0200

    Create color.js

---
+++ constants/color.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    white: "#FFFFFF",
+    black: "#222222",
+    primary: "#007260",
+    secondary: "#39B68D",
+    grey: "#CCCCCC",
+    darkGrey: '#5A5A5A'
+}
+
+export default COLORS;
